e27a5078231fa4b600dba9269bd259b97aaab1eb
D-Baze <wadjakorn.t@mvisioncorp.com>
2017-07-05 09:13:42 +0700

    first commit

---
+++ assets/js/Simple-Slider.js
@@ -0,0 +1,19 @@
+$(function(){
+
+    // Initializing the swiper plugin for the slider.
+    // Read more here: http://idangero.us/swiper/api/
+    
+    var mySwiper = new Swiper ('.swiper-container', {
+        speed: 400,
+        spaceBetween: 100,
+        loop: true,
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        centeredSlides: true,
+        autoplay: 2500,
+        autoplayDisableOnInteraction: false
+    });
+    
+});
